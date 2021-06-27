c32729837bf1db9bb9c646ea1d5f903007ee18b2
GraceYu <librayuyueran@gmail.com>
2021-06-27 14:30:55 -0500

    npm i redux-logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App.js";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {counterReducer} from './reducer'
 import {Provider} from "react-redux";
+import {logger} from 'redux-logger';
+
+// the simple way to create the middleware function
+const myLogger = (store) => (next) => (action) => {
+  return next(action)
+}
+// the complex way to create the middleware function
+// const myLogger = (store) => {
+//   return (next) => {
+//     return (action) => {
+//       console.log('middleware ran')
+//       return next(action)
+//     }
+//   }
+// }
+
+const capAtTen = (store) => (next) => (action) => {
+  if (store.getState() >= 10) {
+    return next({type: 'DECREMENT'})
+  }
+  next(action)
+}
 
 
-const store = createStore(counterReducer)
+const store = createStore(counterReducer, applyMiddleware(myLogger, capAtTen, logger))
 
 ReactDOM.render(
     <Provider store={store}><App/></Provider>,
